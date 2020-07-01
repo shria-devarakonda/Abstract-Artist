837784d067fac861785b73b6cf376c012ebb9a4a
Shria Devarakonda <54900370+shria-devarakonda@users.noreply.github.com>
2020-07-01 15:40:25 +0530

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -7,5 +7,6 @@ setup(500, 500)
 t.hideturtle()
 t.speed(0)
 for i in range(500):
+# try forward(i) or forward(2 * i) for more intricate stuff
     t.forward(5 * i)
     t.right(random.randrange(50, 360, 5))
