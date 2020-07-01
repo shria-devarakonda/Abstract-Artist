b0342abb4568b852faa8de7606784f7c8e2c11fb
Shria Devarakonda <54900370+shria-devarakonda@users.noreply.github.com>
2020-07-01 15:41:27 +0530

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -9,4 +9,4 @@ t.speed(0)
 for i in range(500):
 # try forward(i) or forward(2 * i) for more intricate stuff
     t.forward(5 * i)
-    t.right(random.randrange(50, 360, 5))
+    t.right(random.randrange(50, 180, 5))
